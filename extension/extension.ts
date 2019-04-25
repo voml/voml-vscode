70f0504cc6934ed14aaf02648ea2690266e21070
Aster <galaster@foxmail.com>
2019-04-25 15:57:38 +0800

    🔨 Build Version 0.1.1

---
+++ extension/extension.ts
@@ -0,0 +1,29 @@
+import { writeFileSync } from 'fs'
+import { data, comment, table, key_value, illegal } from '../source'
+function including(_: string) { return { include: '#' + _ } }
+
+const syntax = {
+    version: 'v0.1.1',
+    scopeName: 'source.arc',
+    uuid: '6267dad2-7d52-462a-a1ef-7e3da7378a7d',
+    information_for_contributors: [
+        'aster: galaster@foxmail.com',
+    ],
+    patterns: [
+        including('comment'),
+        including('table'),
+        including('key_value'),
+    ],
+    repository: {
+        comment: comment,
+        table: table,
+        key_value: key_value,
+        data: data,
+        illegal: illegal,
+    },
+}
+
+writeFileSync(
+    __dirname + '/arc.tmLanguage.json',
+    JSON.stringify(syntax, null, 4),
+)
